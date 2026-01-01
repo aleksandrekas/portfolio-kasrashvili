c990b162a9371c850f60b5bab96b703c6eee4807
aleksandre <aleksandrekasrashvili@gmail.com>
2026-01-01 20:54:36 +0400

    added projects component with hovers and buttons

--- src/assets/components/projects/Projects.tsx
+++ src/assets/components/projects/Projects.tsx
@@ -9,6 +9,46 @@ export default function Projects(){
                 <h1>Projects</h1>
                 <button>CONTACT ME</button>
             </header>
+            <div className="projects">
+                <Project
+                    img='/images/thumbnail-project-2-small.webp'
+                    title='just a regular '
+                    tools={['HTML','CSS']}
+                />
+                <Project
+                    img='/images/thumbnail-project-2-small.webp'
+                    title='just a regular everyday'
+                    tools={['HTML','CSS']}
+                />
+            </div>
         </div>
     )
 }
+
+
+
+
+
+
+function Project({img,title,tools}:{img:string,title:string,tools:Array<string>}){
+    return(
+        <div className='project'>
+            <div className='imgContainer'>
+                <img className='projectImage' src={img} alt="project_screenshot" />
+                <div className="buttons">
+                    <a href="http://" target='_blank'>VIEW PROJECT</a>
+                    <a href="http://" target='_blank'>VIEW CODE</a>
+                </div>
+            </div>
+            <p className='projectTitle'>{title}</p>
+            <div className='tools'>
+                {
+                    tools.map((item,index)=>(
+                        <div key={index}>{item}</div>
+                    ))
+                }    
+            </div>
+        </div>
+    )
+}
+
